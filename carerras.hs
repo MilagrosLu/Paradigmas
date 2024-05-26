a4a0bb8aca9d16fac87c3526b8b531ab85080431
MilagrosLu <msalafia@frba.utn.edu.ar>
2024-05-26 16:37:53 -0300

    Ultimos puntos

--- carerras.hs
+++ carerras.hs
@@ -26,7 +26,7 @@ puesto :: Auto -> Carrera -> Int
 puesto unAuto = (1 +) . length . autosQueLeGanan unAuto 
 
 vaTranquilo :: Auto -> Carrera -> Bool
-vaTranquilo unAuto unaCarrera = all (not . estaCerca unAuto)  unaCarrera && puesto unAuto unaCarrera == 1 
+vaTranquilo unAuto unaCarrera = (not . any (estaCerca unAuto)) unaCarrera && puesto unAuto unaCarrera == 1 
 
 type NuevoAuto = Auto -> Auto 
 --PUNTO 2   
@@ -58,6 +58,7 @@ jetPack duracion autoGatillado = afectarALosQueCumplen ((color autoGatillado ==)
     where 
         efectoJetPack duracion unAuto =  alterarVelocidad (const (velocidad unAuto))  . correr duracion . alterarVelocidad (*2) $  unAuto
 
+
 --Punto 4
 
 type Evento = Carrera -> Carrera 
@@ -87,3 +88,39 @@ usarPowerups colorAuto powerUp unaCarrera = powerUp auto unaCarrera
         auto = (\ [x] -> x) . filter ((== colorAuto) . color) $ unaCarrera
 
 -- tirar excepcion si hay más de un auto del mimso color algo que no puede pasar
+
+rojo :: Auto 
+rojo = Auto Rojo 120 0
+
+azul :: Auto
+azul = Auto Azul 120 0
+
+blanco :: Auto
+blanco = Auto Blanco 120 0
+
+negro :: Auto
+negro = Auto Negro 120 0
+
+eventosCarrera1 :: [Evento]
+eventosCarrera1 = [correnTodos 30 , usarPowerups Azul (jetPack 30), usarPowerups Blanco terremoto, correnTodos 40, usarPowerups Blanco (miguelito 20),usarPowerups Negro (jetPack 6), correnTodos 10 ]
+
+carrera1 :: [Auto]
+carrera1 = [rojo, azul, blanco, negro]
+
+simularCarrera1 :: [(Int, Color)]
+simularCarrera1 = simularCarrera carrera1 eventosCarrera1
+
+--PUNTO 5 
+{-
+a. Si es posioble agergar un nuevo Powerup solo sebe añadirse a la solución esa función que 
+recibira un color y debolvera el powerUp para activarlo a un determinado auto, 
+se usar la funcion usarPorwerUp.-}
+
+{-
+b. La funcion puesto no podria evaluarse con una lista infinta de autos ya que no se podria analizar 
+la distancias recorridas por todos. 
+La funcion vaTranquilo solo podrá devolver un resultado en el caso en que encuentre algún auto
+el cual esta cerca lo que permitiria que estaCerca pueda ser evaluada a pesar de ser infinitos autos. 
+La composición con not temina dando un False que obliga a funcion a dar False sin tener que analizar 
+lo que sigue al &&. Esto es posible por la Lazy Evaluation que tiene haskell que va evaluando 
+lo necesario -}
