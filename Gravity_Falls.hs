634d453b9ef5cddd3e4fa762610a18cf84ece8c7
MilagrosLu <msalafia@frba.utn.edu.ar>
2024-05-27 20:18:46 -0300

    terminar

--- Gravity_Falls.hs
+++ Gravity_Falls.hs
@@ -1,7 +1,7 @@
 import Prelude
 import Text.Show.Functions
 import Data.List
---PUNTO 1
+--PUNTO 1 en 1hs 15 min
 data Item = DisfrasDeOveja | SopladorDeHojas deriving (Show, Eq)
 data Persona = Persona {
     edad :: Int, 
@@ -71,10 +71,45 @@ tenerExperiencia unaCondicion = unaCondicion . experiencia
 grupoCriaturas1 :: [Criatura]
 grupoCriaturas1 = [siempreDetras, gnomos 10, fantasmas 3 [tenerEdad (<13), tieneItem DisfrasDeOveja] , fantasmas 1 [tenerExperiencia (>10)]]
 
+grupoCriaturasEj :: [Criatura]
 grupoCriaturasEj = [siempreDetras, gnomos 10]
 
+dipper :: Persona
 dipper = Persona {
     edad = 13, 
     experiencia = 5,
-    items = [DisfrasDeOveja, SopladorDeHojas]
+    items = [DisfrasDeOveja]
 }
+
+--PARTE 2 
+zipWithIf :: (a -> b -> b) -> (b -> Bool) -> [a] -> [b] -> [b] 
+zipWithIf _ _ [] [] = []
+zipWithIf _ _ (x:xs) [] = []
+zipWithIf _ _ [] (y:ys) = []
+zipWithIf operacion unaCondicion (x:xs) (y:ys) 
+    | not . unaCondicion $ y = y : zipWithIf operacion unaCondicion (x:xs) ys 
+    | otherwise = operacion x y :  zipWithIf operacion unaCondicion xs ys 
+
+abecedario :: [Char]
+abecedario = ['a'..'z']
+
+abecedarioDesde :: Char -> [Char]
+abecedarioDesde letraComienzo = dropWhile (/= letraComienzo) abecedario ++ init ['a' .. letraComienzo ]
+
+desencriptarLetra :: Char -> Char -> Char
+desencriptarLetra letraClave letraEncriptada = fst  . head . dropWhile ((/= letraEncriptada) . snd) .  zip abecedario . abecedarioDesde $ letraClave
+
+listaClaveRepetida :: String -> String -> String 
+listaClaveRepetida clave mensajeEncriptado = concat (replicate (div (length mensajeEncriptado) (length clave) + 1) clave) 
+
+desencriptarFrase :: [Char] -> [Char] -> [Char]
+desencriptarFrase clave mensajeEncriptado = zipWithIf desencriptarLetra (`elem` abecedario) (listaClaveRepetida clave mensajeEncriptado) mensajeEncriptado
+
+vigenere :: String -> String -> String 
+vigenere  = desencriptarFrase 
+
+cesar :: Char -> String -> String
+cesar letraClave  = desencriptarFrase [letraClave]
+
+todasLasDesencriptaciones :: String -> [String]
+todasLasDesencriptaciones unMensaje = map (`cesar` unMensaje) abecedario
